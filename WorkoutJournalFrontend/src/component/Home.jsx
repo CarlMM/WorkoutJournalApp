1b21adcb264122b8d0ee26a8326f3eafda327b5f
CarlMM <kaptenkeps@gmail.com>
2022-03-11 18:04:11 +0100

    Simple routing, seperate css file, good structure

---
+++ WorkoutJournalFrontend/src/component/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../cssFolder/HomeCss.css'
+
+function Home(){
+    const welcomeMessage = "This is the HomePage"
+
+
+    return <>
+    <h1>{welcomeMessage}</h1>
+    </>
+}
+
+export default Home
