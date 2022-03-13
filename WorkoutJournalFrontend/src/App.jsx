24453422b49aefa3e9159bbb9a080058880052de
CarlMM <kaptenkeps@gmail.com>
2022-03-13 19:49:00 +0100

    Made navbar component work

--- WorkoutJournalFrontend/src/App.jsx
+++ WorkoutJournalFrontend/src/App.jsx
@@ -1,52 +1,53 @@
 import React, {useState, useEffect} from 'react'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom"
 
-import Home from './component/Home';
-import MyProfilePage from './component/MyProfile/MyProfilePage'
-//import Navbar from './component/Navbar'
+//import Home from './component/Home';
+//import MyProfilePage from './component/MyProfile/MyProfilePage'
+import Navbar from './component/Navbar'
 
 
 
 
 function App() {
   
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((<p>It is {new Date().toLocaleTimeString()}.</p>));
-    }, 1000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((<p>It is {new Date().toLocaleTimeString()}.</p>));
+  //   }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
     
     return (
-      <Router>
-      <div>
-        <div class="topNav">
-          <nav>
-            {/* {tick()} */}
-            {counter}
-            <Link to="/">Home</Link>
-            <Link to="/myProfilePage">My Profile</Link>
-          </nav>
-        </div>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/myProfilePage">
-            <MyProfilePage/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      <Navbar/>
+    //   <Router>
+    //   <div>
+    //     <div class="topNav">
+    //       <nav>
+    //         {/* {tick()} */}
+    //         {counter}
+    //         <Link to="/">Home</Link>
+    //         <Link to="/myProfilePage">My Profile</Link>
+    //       </nav>
+    //     </div>
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <Home/>
+    //       </Route>
+    //       <Route path="/myProfilePage">
+    //         <MyProfilePage/>
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
   )
 }
 
--- WorkoutJournalFrontend/src/component/Navbar.jsx
+++ WorkoutJournalFrontend/src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,11 +7,27 @@ import {
 } from "react-router-dom"
 
 
+import Home from './Home'
+import MyProfilePage from './MyProfile/MyProfilePage'
+
 function Navbar(){
+
+ const [counter, setCounter] = useState(0);
+
+   useEffect(() => {
+     const interval = setInterval(() => {
+       setCounter((<p>It is {new Date().toLocaleTimeString()}.</p>));
+     }, 1000);
+
+     return () => clearInterval(interval);
+   }, []);
+
+  return(
     <Router>
-      <div>
+    <div>
         <div class="topNav">
           <nav>
+            {counter}
             <Link to="/">Home</Link>
             <Link to="/myProfilePage">My Profile</Link>
           </nav>
@@ -25,7 +41,9 @@ function Navbar(){
           </Route>
         </Switch>
       </div>
-    </Router>
+   </Router>
+
+  )
 }
 
 
