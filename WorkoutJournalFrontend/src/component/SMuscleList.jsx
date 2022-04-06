423a58aa65342fb929fad8b14bbc0f9acd0ed820
CarlMM <kaptenkeps@gmail.com>
2022-04-06 19:57:52 +0200

    now properly loops through filtered array of muscles
    Signed-off-by: CarlMM <kaptenkeps@gmail.com>

---
+++ WorkoutJournalFrontend/src/component/SMuscleList.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+
+function SMuscleList(props){
+
+
+    return(
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+export default SMuscleList;
--- WorkoutJournalFrontend/src/component/SpecificMuscle.jsx
+++ WorkoutJournalFrontend/src/component/SpecificMuscle.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import SMuscleList from "./SMuscleList";
+
 
 function SpecificMuscle(props){
 
 
     let {id} = useParams();
 
-    const checkMuscle = () =>{
-        console.log(props.sMuscle)
+    const filterMuscleExercises = () => {
+        const newList = props.sMuscle.filter(sM => sM.muscleCateogryId == id)
+        console.log(newList)
     }
 
     return(
@@ -16,11 +19,12 @@ function SpecificMuscle(props){
             <h1>Muscle id</h1>
             <h2>Muscle Details</h2>
             <h3>id: {id}</h3>
-            
+            <ul>
+                {props.sMuscle.filter(s => s.muscleCateogryId == id)
+                .map(filteredArray => (<li>{filteredArray.title}</li>))}
+            </ul>
         </div>
     )
-
-
 
 }
 
