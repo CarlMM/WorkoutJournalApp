fc6580da672a9711b8d9b9ef0d0aad591464f658
CarlMM <kaptenkeps@gmail.com>
2022-05-12 11:37:55 +0200

    Started trying to implement generic interface

---
+++ WorkoutJournalWebApi/WorkoutJournal.Domain/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutJournal.Domain.Models;
+
+namespace WorkoutJournal.Domain.RepositoryInterfaces
+{
+    public interface IRepository<T>
+    {
+        //Task <IEnumerable<T>> GetAllItems();
+
+        //Task<T> GetSpecificItem(int id);
+
+        //Task<T> AddNewItem(T itemToAdd);
+
+        //Task<T> RemoveSpecificItem(T itemToRemove);
+
+        //Task SaveChangesAsync();
+
+    }
+}
